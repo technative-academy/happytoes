1b7ed3188be520ba8c53b49663922a386efba23f
Yorick <info@theyoricktouch.com>
2025-10-09 13:16:13 +0100

    Tabs js, css, html updates

---
+++ scripts/tabs-module.js
@@ -0,0 +1,37 @@
+class TabsModule {
+	constructor(selector) {
+		this.tabsContainer = document.querySelector(selector);
+
+		this.tabButtons = this.tabsContainer.querySelectorAll(".tabs__item");
+
+		this.tabContents = this.tabsContainer.querySelectorAll(".tab__content");
+		this.tabButtons.forEach((tabButton) => {
+			tabButton.addEventListener("click", (event) =>
+				this.tabClicked(event)
+			);
+		});
+	}
+
+	tabClicked(event) {
+		const clickedButton = event.currentTarget;
+		const tabId = clickedButton.dataset.tab;
+
+		const detailsElement = this.tabsContainer.querySelector(
+			`.tabs__content[data-tab-content="${tabId}"]`
+		);
+
+		// Remove active class from all buttons and content
+		this.tabButtons.forEach((button) => button.classList.remove("active"));
+		this.tabContents.forEach((content) =>
+			content.classList.remove("active")
+		);
+
+		// Add active class to clicked button and corresponding content
+		clickedButton.classList.add("active");
+		if (detailsElement) {
+			detailsElement.classList.add("active");
+		}
+	}
+}
+
+export default TabsModule;
