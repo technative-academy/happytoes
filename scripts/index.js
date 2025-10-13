e5e481191ba86f6452f9648179e66aa8acd9df26
YashMagane <yash.magane.11@gmail.com>
2025-10-13 14:31:13 +0100

    review section complete

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
 import TabsModule from "./tabs-module.js";
+import ReviewTabsModule from "./review-tabs-module.js";
 
 const tabsOne = new TabsModule(".tabs");
+const reviewTabs = new ReviewTabsModule(".review__section");
--- scripts/tabs-module.js
+++ scripts/tabs-module.js
@@ -1,38 +1,36 @@
 class TabsModule {
-	constructor(selector) {
-		this.tabsContainer = document.querySelector(selector);
+  constructor(selector) {
+    this.tabsContainer = document.querySelector(selector)
 
-		this.tabButtons = this.tabsContainer.querySelectorAll(".tabs__item");
+    this.tabButtons = this.tabsContainer.querySelectorAll(".tabs__item")
 
-		this.tabContents = this.tabsContainer.querySelectorAll(".tabs__content__item");
+    this.tabContents = this.tabsContainer.querySelectorAll(
+      ".tabs__content__item"
+    )
 
-		this.tabButtons.forEach((tabButton) => {
-			tabButton.addEventListener("click", (event) =>
-				this.tabClicked(event)
-			);
-		});
-	}
+    this.tabButtons.forEach((tabButton) => {
+      tabButton.addEventListener("click", (event) => this.tabClicked(event))
+    })
+  }
 
-	tabClicked(event) {
-		const clickedButton = event.currentTarget;
-		const tabId = clickedButton.dataset.tab;
+  tabClicked(event) {
+    const clickedButton = event.currentTarget
+    const tabId = clickedButton.dataset.tab
 
-		const detailsElement = this.tabsContainer.querySelector(
-			`.tabs__content__item[data-tab-content="${tabId}"]`
-		);
+    const detailsElement = this.tabsContainer.querySelector(
+      `.tabs__content__item[data-tab-content="${tabId}"]`
+    )
 
-		// Remove active class from all buttons and content
-		this.tabButtons.forEach((button) => button.classList.remove("active"));
-		this.tabContents.forEach((content) =>
-			content.classList.remove("active")
-		);
+    // Remove active class from all buttons and content
+    this.tabButtons.forEach((button) => button.classList.remove("active"))
+    this.tabContents.forEach((content) => content.classList.remove("active"))
 
-		// Add active class to clicked button and corresponding content
-		clickedButton.classList.add("active");
-		if (detailsElement) {
-			detailsElement.classList.add("active");
-		}
-	}
+    // Add active class to clicked button and corresponding content
+    clickedButton.classList.add("active")
+    if (detailsElement) {
+      detailsElement.classList.add("active")
+    }
+  }
 }
 
-export default TabsModule;
+export default TabsModule
